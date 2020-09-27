f8fa333eb8840f62ef8864708522e3440628996d
burkoo <32197252+burkoo@users.noreply.github.com>
2020-09-27 21:34:44 +0300

    listing with spring boot method naming, also result with DTO

--- src/main/java/com/aegon/interviewproject/api/answer/controller/AnswerController.java
+++ src/main/java/com/aegon/interviewproject/api/answer/controller/AnswerController.java
@@ -3,6 +3,7 @@ package com.aegon.interviewproject.api.answer.controller;
 import com.aegon.interviewproject.api.answer.controller.dto.AnswerDTO;
 import com.aegon.interviewproject.api.answer.controller.dto.AnswerResultDTO;
 import com.aegon.interviewproject.api.answer.controller.mapper.AnswerMapper;
+import com.aegon.interviewproject.api.answer.controller.mapper.AnswerResultMapper;
 import com.aegon.interviewproject.api.answer.repository.domain.Answer;
 import com.aegon.interviewproject.api.answer.service.AnswerService;
 import com.aegon.interviewproject.api.survey.repository.domain.Survey;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -25,6 +27,9 @@ public class AnswerController {
     private AnswerMapper answerMapper;
 
     @Autowired
+    private AnswerResultMapper answerResultMapper;
+
+    @Autowired
     private SurveyService surveyService;
 
     @PostMapping("/add")
@@ -33,6 +38,7 @@ public class AnswerController {
         return answerService.save(answerMapper.toEntity(answerDTO));
     }
 
+    // using querydsl
     @GetMapping("/list/{topicId}")
     @ResponseStatus(HttpStatus.OK)
     private List<Answer> list(@PathVariable UUID topicId){
@@ -41,8 +47,13 @@ public class AnswerController {
 
     @GetMapping("/list2/{topicId}")
     @ResponseStatus(HttpStatus.OK)
-    private List<Answer> list2(@PathVariable UUID topicId){
+    private List<AnswerResultDTO> list2(@PathVariable UUID topicId){
         Survey survey = surveyService.findById(topicId);
-        return answerService.findBySurvey(survey);
+        List<Answer> answerList = answerService.findBySurvey(survey);
+        List<AnswerResultDTO> result = new ArrayList<>();
+        for(Answer answer : answerList){
+            result.add(answerResultMapper.toDTO(answer));
+        }
+        return result;
     }
 }
