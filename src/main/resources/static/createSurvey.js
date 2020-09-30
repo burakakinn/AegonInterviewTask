fdf86ee80d414405feed29b763c70d69a656440e
burkoo <32197252+burkoo@users.noreply.github.com>
2020-09-30 17:06:58 +0300

    submit page left only

---
+++ src/main/resources/static/createSurvey.js
@@ -0,0 +1,12 @@
+var baseurl = "http://localhost:8080/survey";
+function addSurvey(){
+    var survey = {
+        topic: document.getElementById("topicInput").value,
+        question: document.getElementById("questionInput").value
+    };
+    var json = JSON.stringify(survey);
+    var httprequest = new XMLHttpRequest();
+    httprequest.open("POST", baseurl + "/add",true);
+    httprequest.setRequestHeader("Content-Type", "application/json");
+    httprequest.send(json);
+}
---
+++ src/main/resources/static/submitSurvey.js
@@ -0,0 +1,49 @@
+var baseurlSurvey = "http://localhost:8080/survey";
+var baseurlAnswer = "http://localhost:8080/answer";
+var score = 0;
+function loadTopics(){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET",baseurlSurvey + "/allResults",true);
+    xmlhttp.onreadystatechange = function() {
+        if(xmlhttp.readyState ===4 && xmlhttp.status ===200){
+            var results = JSON.parse(xmlhttp.responseText);
+            var top = `<div class="input-group-prepend"><label class="input-group-text" for="inputGroupSelect01">CHOOSE TOPIC</label>
+                    </div><select onchange="loadFields(value);" style="height: 40px" class="custom-select" id="inputGroupSelect01"><option selected>Choose...</option>`;
+            //main table content we fill from data from the rest call
+            var main ="";
+            for (i = 0; i < results.length; i++){
+                main += "<option value=\""+results[i].topicId+"\">" + results[i].topicName + "</option>";
+            }
+            var bottom = "</select>";
+            var customSelect = top + main + bottom;
+            document.getElementById("allTopics").innerHTML = customSelect;
+        }
+    };
+    xmlhttp.send();
+}
+
+function loadFields(id){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET",baseurlSurvey + "/"+id,true);
+    xmlhttp.onreadystatechange = function() {
+        if(xmlhttp.readyState ===4 && xmlhttp.status ===200){
+            var result = JSON.parse(xmlhttp.responseText);
+            document.getElementById("topic").innerHTML = "<p>How likely are you to recommend " + result.topic +" to a friend or colleague?</p>";
+            document.getElementById("question").innerHTML = "<p>"+result.question+"</p>";
+        }
+    };
+    xmlhttp.send();
+}
+
+window.onload = function(){
+    loadTopics();
+}
+
+function submitSurvey(){
+    var answer = document.getElementById("answerInput").value;
+
+}
+
+function setScore(scoreInput){
+    score = scoreInput;
+}
